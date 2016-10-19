aef953c2b154e81da62210f816aaa5a308a61576
obrunovieira <bruno.vieira@concretesolutions.com.br>
2016-10-19 19:37:45 -0200

    added register flow

---
+++ app/src/main/java/com/brunovieira/finco/presentation/presenter/interfaces/RegisterPresenter.kt
@@ -0,0 +1,12 @@
+package com.brunovieira.finco.presentation.presenter.interfaces
+
+import com.brunovieira.finco.presentation.ui.interfaces.LoginView
+import com.brunovieira.finco.presentation.ui.interfaces.RegisterView
+
+/**
+ * Created by bruno.vieira on 19/10/2016.
+ */
+
+interface RegisterPresenter {
+    fun attachView(registerView: RegisterView)
+}
---
+++ app/src/main/java/com/brunovieira/finco/presentation/ui/interfaces/RegisterView.kt
@@ -0,0 +1,9 @@
+package com.brunovieira.finco.presentation.ui.interfaces
+
+/**
+ * Created by bruno.vieira on 19/10/2016.
+ */
+interface RegisterView {
+
+    open fun setupActionBar()
+}
