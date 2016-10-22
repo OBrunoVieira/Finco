2c56a6a169bbf975db14d432f3a1c29f871a219c
obrunovieira <bruno.vieira@concretesolutions.com.br>
2016-10-21 22:31:18 -0200

    added home flow

---
+++ app/src/main/java/com/brunovieira/finco/presentation/presenter/HomePresenterImpl.kt
@@ -0,0 +1,5 @@
+package com.brunovieira.finco.presentation.presenter
+
+/**
+ * Created by bruno.vieira on 21/10/2016.
+ */
---
+++ app/src/main/java/com/brunovieira/finco/presentation/ui/interfaces/HomeView.kt
@@ -0,0 +1,5 @@
+package com.brunovieira.finco.presentation.ui.interfaces
+
+/**
+ * Created by bruno.vieira on 21/10/2016.
+ */
